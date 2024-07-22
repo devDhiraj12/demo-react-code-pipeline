0f4a1fa9e775d62c65417961417802d2c7ed1ea6
devDhiraj12 <dhiraj@quickinfracloud.com>
2024-07-22 15:26:51 +0530

    buildspec

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
-import MyBanner from './MyBanner';
+import React, { Component } from "react";
+import "./App.css";
+import Hello from "./Hello";
 
 class App extends Component {
-
   render() {
-
-    const myAddress = {
-      street: 'Somewhere',
-      city: 'SLC',
-      state: 'UT',
-      zip: '12334'
-    };
-
     return (
       <div>
-        <MyBanner
-          name="Tyler"
-          address={myAddress}
-        />
+        <Hello />
       </div>
     );
   }
---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function hello() {
+  return (
+    <div>hello from react app</div>
+  )
+}
+
+export default hello
